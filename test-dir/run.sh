b9ef91d7a51a39e8429c70146f0f07e5df73b330
Ubuntu <ubuntu@ip-172-31-27-158.ec2.internal>
2023-03-16 18:07:04 +0000

    updated

--- test-dir/run.sh
+++ test-dir/run.sh
@@ -2,6 +2,7 @@ kubectl apply -f frontend-tcp-service.yaml
 kubectl apply -f redis-master-service.yaml
 kubectl apply -f redis-slave-service.yaml
 kubectl apply -f frontend-deployment.yaml
+kubectl apply -f test-dir-default-networkpolicy.yaml
 kubectl apply -f redis-master-deployment.yaml
 kubectl apply -f redis-slave-deployment.yaml
 
