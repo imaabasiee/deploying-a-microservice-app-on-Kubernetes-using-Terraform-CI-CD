6c31d253e5edc4bb771f228558b0465030d433bf
Ima-Abasi E. Effiong <imaabasieea@gmail.com>
2023-03-16 13:15:06 +0000

    updated

---
+++ test-dir/kompose.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Linux
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.28.0/kompose-linux-amd64 -o kompose
+
+chmod +x kompose
+sudo mv ./kompose /usr/local/bin/kompose
